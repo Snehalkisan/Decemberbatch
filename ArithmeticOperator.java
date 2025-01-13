7ac37d76d528cbf14f2671068545dcd4ea206f07
Snehal <kadamsnehalk18@gmail.com>
2025-01-13 13:54:24 +0530

    java

---
+++ ArithmeticOperator.java
@@ -0,0 +1,15 @@
+public class ArithmeticOperator {
+    public static void main(String []args)
+    {
+        // to perform operations use arithmetic operators +,-,*,/,% for
+        // addition,subtraction,multiplication,quotient ,remainder
+
+        int a=10;
+        int b=6;
+        System.out.println("Sum="+(a+b));
+        System.out.println("Difference="+(a-b));
+        System.out.println("Product="+(a*b));
+        System.out.println("Quotient="+(a/b));
+        System.out.println("Remainder="+(a%b));
+    }
+}
